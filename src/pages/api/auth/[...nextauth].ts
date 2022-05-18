8fa1425bda48c17267a3476db34fd9df5b1cc7f8
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-05-18 08:55:02 -0300

    fixed next-auth v4 issues

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+})
